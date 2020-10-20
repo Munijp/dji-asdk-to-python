b588d75e775fc5304e1e10538455c824dae549e1
JuanDavid69 <juan.gomez.orozco@correounivalle.edu.co>
2020-10-20 16:28:38 -0500

    add battery class and battery state class

---
+++ examples/get_battery_state.py
@@ -0,0 +1,10 @@
+from dji_asdk_to_python.products.aircraft import Aircraft
+from dji_asdk_to_python.battery.battery_state import BatteryState
+
+APP_IP = "192.168.0.174"
+
+drone = Aircraft(APP_IP)
+battery = drone.getBattery()
+battery_state = battery.getBatteryState()
+
+print("battery state %s " % battery_state.getChargeRemainingInPercent())
