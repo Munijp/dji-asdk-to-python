c344b7355af30915dc0a8d1c261b391427167dd1
davidbo99 <david-702bo@hotmail.com>
2021-08-30 18:05:59 -0400

    Fixed iso settings

--- dji_asdk_to_python/precision_landing/landing.py
+++ dji_asdk_to_python/precision_landing/landing.py
@@ -412,8 +412,8 @@ class ArucoLanding:
             camera.setISO(ISO.ISO_400)
             camera.setShutterSpeed(ShutterSpeed.SHUTTER_SPEED_1_2000)
         elif cameraType == "EVENING":
-            camera.setISO(ISO.ISO_400)
-            camera.setShutterSpeed(ShutterSpeed.SHUTTER_SPEED_1_8000)
+            camera.setISO(ISO.ISO_800)
+            camera.setShutterSpeed(ShutterSpeed.SHUTTER_SPEED_1_2000)
         elif cameraType == "MORNING":
             camera.setISO(ISO.ISO_400)
             camera.setShutterSpeed(ShutterSpeed.SHUTTER_SPEED_1_4000)
